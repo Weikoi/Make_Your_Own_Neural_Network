ae28a962bf3e1ef587c6c259dab03f2a9d0b8b83
huangxiaowei <huangzengrui126@126.com>
2019-07-30 14:11:46 +0800

    modify

--- keras/imdb_demo.py
+++ keras/imdb_demo.py
@@ -1,14 +1,15 @@
+
+#%%
 from keras.datasets import imdb
 import numpy as np
 from keras import optimizers
 from keras import losses
 from keras import metrics
-
 from keras import models
 from keras import layers
-
 import matplotlib.pyplot as plt
 
+#%%
 (train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=10000)
 
 word_index = imdb.get_word_index()
@@ -22,12 +23,13 @@ def vectorize_sequences(sequences, dimension=10000):
         results[i, sequence] = 1.
     return results
 
-
+#%%
 x_train = vectorize_sequences(train_data)
 x_test = vectorize_sequences(test_data)
 y_train = np.asarray(train_labels).astype('float32')
 y_test = np.asarray(test_labels).astype('float32')
 
+#%%
 model = models.Sequential()
 model.add(layers.Dense(16, activation='relu', input_shape=(10000,)))
 model.add(layers.Dense(16, activation='relu'))
@@ -40,6 +42,7 @@ model.compile(optimizer=optimizers.RMSprop(lr=0.001), loss='binary_crossentropy'
 model.compile(optimizer=optimizers.RMSprop(lr=0.001), loss=losses.binary_crossentropy,
               metrics=[metrics.binary_accuracy])
 
+#%%
 x_val = x_train[:10000]
 partial_x_train = x_train[10000:]
 y_val = y_train[:10000]
@@ -50,16 +53,44 @@ history = model.fit(partial_x_train,
                     epochs=20,
                     batch_size=512,
                     validation_data=(x_val, y_val))
+#%%
+history_dict = history.history
+print(history_dict.keys())
+print(history_dict.values())
 
-# acc = history.history['acc']  # 获取训练集准确性数据
-# val_acc = history.history['val_acc']  # 获取验证集准确性数据
-# loss = history.history['loss']  # 获取训练集错误值数据
-# val_loss = history.history['val_loss']  # 获取验证集错误值数据
-# epochs = range(1, len(acc) + 1)
-# plt.plot(epochs, acc, 'bo', label='Trainning acc')  # 以epochs为横坐标，以训练集准确性为纵坐标
-# plt.plot(epochs, val_acc, 'b', label='Vaildation acc')  # 以epochs为横坐标，以验证集准确性为纵坐标
-# plt.legend()  # 绘制图例，即标明图中的线段代表何种含义
-#
-# plt.figure()  # 创建一个新的图表
-# plt.plot(epochs, loss, 'bo', label='Trainning loss')
-# plt.plot(epochs, val_loss, 'b', label='Vaildation loss')
+#%%
+import matplotlib.pyplot as plt
+
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(1, len(acc) + 1)
+
+# "bo" is for "blue dot"
+plt.plot(epochs, loss, 'bo', label='Training loss')
+# b is for "solid blue line"
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+
+plt.show()
+
+#%%
+plt.clf()   # clear figure
+acc_values = history_dict['acc']
+val_acc_values = history_dict['val_acc']
+
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+
+plt.show()
+
+
